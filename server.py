12b33d29b095f0d3a59d95265532263850d08b21
khalilzheng <165823328+khalilzheng@users.noreply.github.com>
2025-04-23 11:14:21 -0400

    Update server.py

--- server.py
+++ server.py
@@ -34,9 +34,27 @@ quiz_data = {
     ]
 }
 
-@app.route('/')
-def mainpage():
-    return render_template('mainpage.html')  
+def load_lessons():
+    with open("static/lessons.json") as f:
+        data = json.load(f)
+        return list(data.values()) 
+
+@app.route("/")
+def start():
+    return render_template("start.html")
+
+@app.route("/home")
+def home():
+    lessons = load_lessons()
+    return render_template("home.html", lessons=lessons)
+
+@app.route("/learn/<int:lesson_id>")
+def learn(lesson_id):
+    with open("static/lessons.json") as f:
+        data = json.load(f)
+        lesson = data[str(lesson_id)]  
+        return render_template("learn.html", lesson=lesson)
+
 
 @app.route('/quiz')
 def quiz():
